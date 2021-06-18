36422f1878ced04e5563d140e1c9bc25dc77bd7a
Juan Diego <juandiego.mbr@gmail.com>
2021-06-18 14:52:23 +0200

    Clean the project and copy the codesandbox code

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import products from "./products.json";
 
-function App() {
+const App = () => {
+  const [price, setPrice] = useState(0);
+
+  function handlePriceChange(event) {
+    setPrice(event.target.value);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="display">
+        <label>
+          <span>Peso:</span>
+          <input type="number" step="any" min="0" placeholder="0,000" />
+        </label>
+        <label>
+          <span>Precio:</span>
+          <input
+            type="number"
+            step="any"
+            min="0"
+            placeholder="0,000"
+            value={price}
+            onChange={handlePriceChange}
+          />
+        </label>
+        <label>
+          <span>Total:</span>
+          <input type="number" placeholder="0,000" disabled />
+        </label>
+      </div>
+      <div className="controls">
+        <div className="products">
+          {products.map((product) => {
+            return (
+              <button key={product.id} aria-label={product.name}>
+                <img src={product.image} alt="" />
+              </button>
+            );
+          })}
+        </div>
+        <div className="sidebar">
+          <div>
+            <button>Calcular</button>
+          </div>
+          <div>Total - 0,000€</div>
+          <ul>
+            <li>Plátano - 2,570€</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export { App };
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,110 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
-test('renders learn react link', () => {
+import "@testing-library/jest-dom/extend-expect";
+
+import { App } from "./App";
+
+it("should calculate the weight price manually", async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const priceInput = screen.getByLabelText("Precio:");
+  const calculateButton = screen.getByText("Calcular");
+  userEvent.type(weightInput, "2");
+  userEvent.type(priceInput, "5");
+  userEvent.click(calculateButton);
+
+  const totalInput = screen.getByLabelText("Total:");
+  expect(weightInput).toHaveValue(2);
+  expect(priceInput).toHaveValue(5);
+  expect(totalInput).toHaveValue(10);
+});
+
+it.skip("should clear the input values", async () => {
+  render(<App />);
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const priceInput = screen.getByLabelText("Precio:");
+  const calculateButton = screen.getByText("Calcular");
+  const clearButton = screen.getByText("Limpiar");
+  userEvent.type(weightInput, "2");
+  userEvent.type(priceInput, "5");
+  userEvent.click(calculateButton);
+  userEvent.click(clearButton);
+
+  const totalInput = screen.getByLabelText("Total:");
+  expect(weightInput).toHaveValue(0);
+  expect(priceInput).toHaveValue(0);
+  expect(totalInput).toHaveValue(0);
+});
+
+it.skip("should calculate the weight price with the product button", async () => {
+  render(<App />);
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const bananaButton = screen.getByLabelText("Plátano");
+  const calculateButton = screen.getByText("Calcular");
+  userEvent.type(weightInput, "2");
+  userEvent.click(bananaButton);
+  userEvent.click(calculateButton);
+
+  const totalInput = screen.getByLabelText("Total:");
+  expect(totalInput).toHaveValue(3.38);
+});
+
+it.skip("should add the product weight to the list", async () => {
+  render(<App />);
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const bananaButton = screen.getByLabelText("Plátano");
+  const addToListButton = screen.getByText("Añadir");
+  userEvent.type(weightInput, "2");
+  userEvent.click(bananaButton);
+  userEvent.click(addToListButton);
+
+  const item = screen.getByRole("listitem");
+  expect(item).toHaveTextContent("Plátano - 3.380 €");
+});
+
+it.skip("should display the total price of the purchase", async () => {
+  render(<App />);
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const bananaButton = screen.getByLabelText("Plátano");
+  const watermelonButton = screen.getByLabelText("Sandía");
+  const addToListButton = screen.getByText("Añadir");
+  userEvent.type(weightInput, "2");
+  userEvent.click(bananaButton);
+  userEvent.click(addToListButton);
+  userEvent.type(weightInput, "4");
+  userEvent.click(watermelonButton);
+  userEvent.click(addToListButton);
+
+  const [firstItem, secondItem] = screen.getAllByRole("listitem");
+  expect(firstItem).toHaveTextContent("Plátano - 3.380€");
+  expect(secondItem).toHaveTextContent("Sandía - 3.160€");
+  expect(screen.getByText("Total - 6.540€")).toBeInTheDocument();
+});
+
+it.skip("should be able to clean the purchase", async () => {
+  render(<App />);
+
+  const weightInput = screen.getByLabelText("Peso:");
+  const bananaButton = screen.getByLabelText("Plátano");
+  const addToListButton = screen.getByText("Añadir");
+  const resetButton = screen.getByText("Borrar");
+  userEvent.type(weightInput, "2");
+  userEvent.click(bananaButton);
+  userEvent.click(addToListButton);
+  const item = screen.getByRole("listitem");
+  userEvent.click(resetButton);
+
+  const priceInput = screen.getByLabelText("Precio:");
+  const totalInput = screen.getByLabelText("Total:");
+  expect(item).not.toBeInTheDocument();
+  expect(weightInput).toHaveValue(0);
+  expect(priceInput).toHaveValue(0);
+  expect(totalInput).toHaveValue(0);
+  expect(screen.getByText("Total - 0.000€")).toBeInTheDocument();
 });
